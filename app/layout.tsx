2d5b8d9551defa60a90df2a59dcb53879d0643c4
enoabasi <watermelonbit@gmail.com>
2024-03-29 17:08:52 +0400

    feat: Adding vercel site improvements and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
@@ -20,11 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn("relative antialiased", inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          
-    
-    <div className="bg-[radial-gradient(at_20.67%_30.80%,rgb(204,251,241)_0,transparent_12%),radial-gradient(at_83.67%_81.17%,rgb(224,242,254)_0,transparent_28%)] dark:hidden inset-0 -z-10 absolute"></div>
-  
+          <div className="bg-[radial-gradient(at_20.67%_30.80%,rgb(204,251,241)_0,transparent_12%),radial-gradient(at_83.67%_81.17%,rgb(224,242,254)_0,transparent_28%)] dark:hidden inset-0 -z-10 absolute"></div>
           {children}
+          <Analytics />
+          <SpeedInsights />
           <TailwindIndicator />
         </ThemeProvider>
       </body>
